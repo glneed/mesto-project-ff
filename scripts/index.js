56ae0f89669911e2062465a0bc0e81fad81a78c1
glneed <genupmen@gmail.com>
2025-12-23 20:34:03 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+function createCard(cardData, deleteCallback) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+  
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function() {
+    deleteCallback(cardElement);
+  });
+  
+  return cardElement;
+}
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
-// @todo: Функция удаления карточки
+const placesList = document.querySelector('.places__list');
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function(cardData) {
+  const cardElement = createCard(cardData, deleteCard);
+  placesList.append(cardElement);
+});
